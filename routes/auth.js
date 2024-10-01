52902a625b3f2d7d8cdaf272d1e4acc9bd310852
M-Saad-mizri <saadmizri434@gmail.com>
2024-10-01 09:10:26 +0500

    auth added

--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,12 @@ router.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const trimmedEmail = email;
-    const hashedPassword = await bcrypt.hash(password, 10);
+    // const hashedPassword = await bcrypt.hash(password, 7);
 
     const newUser = new User({
       username,
       email: trimmedEmail,
-      password: hashedPassword,
+      password: password,
     });
     await newUser.save();
     res.status(201).redirect("/signin");
@@ -28,15 +28,21 @@ router.post("/signin", async (req, res) => {
   try {
     const user = await User.findOne({ email: trimmedEmail });
     console.log("User found:", user);
+
     if (!user) {
-      return res.status(401).send("user not found");
+      return res.status(401).send("User not found");
     }
+
     const match = await bcrypt.compare(password, user.password);
-    console.log("user password is: " + user.password);
+    console.log("Comparing password for user:", user.email);
+    console.log("Hashed Password:", user.password);
+    console.log("Input Password:", password);
     console.log("Password match:", match);
+
     if (!match) {
       return res.status(401).send("Invalid email or password");
     }
+
     req.session.userId = user._id;
     res.redirect("/");
   } catch (error) {
